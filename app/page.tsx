51a7ed0f0c5d17920145cad7511eeb79f5161897
Nikhil Sharma <nikhilsharma7028@gmail.com>
2024-04-14 18:12:35 +0530

    MobileNav Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Nav from "@/Components/nav";
-import React from 'react';
+'use client';
+
+import MobileNav from "@/Components/MobileNav";
+import Nav from "@/Components/Nav";
+import React, { useState } from "react";
 
 const HomePage = () => {
+  
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
     return (
       <div className="overflow-x-hidden">
         <div>
-          <Nav />
+          {/* <MobileNav nav={nav} closeNav={closeNav} /> */}
+          <Nav openNav={openNav} />
         </div>
       </div>
     );
