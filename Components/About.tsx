8169d388e495c819a3f972e8aa558c38289afb8c
Nikhil Sharma <nikhilsharma7028@gmail.com>
2024-04-21 23:05:14 +0530

    About Updates

--- Components/About.tsx
+++ Components/About.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 const About = () => {
@@ -11,6 +12,20 @@ const About = () => {
                 <h2 className='text-[25px] md:text-[35px] lg:text-[45px] md:leading-[3rem] leading-[2rem] capitalize mb-[3rem] font-bold text-white'>
                     Transforming <span className='text-yellow-400'>Visions</span>
                 </h2>
+                <div className='mb-[3rem] flex items-center md:space-x-10'>
+                    <span className='w-[100px] hidden md:block h-[5px] bg-slate-400 rounded-sm'></span>
+                    <p className='text-[19px] text-slate-300 w-[80%]'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, natus quaerat 
+                        blanditiis reprehenderit obcaecati expedita, quidem dolores consequatur possimus 
+                        architecto deserunt rerum sequi quibusdam esse aut quisquam unde quo numquam.
+                    </p>
+                </div>
+                <button className='px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2'>
+                    <p>Know More</p>
+                </button>
+            </div>
+            <div className='lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[300px] h-[300px] relative'>
+                <Image src="/images/about.jpg" alt='user' layout='fill' objectFit='contain' className='relative z-[11] w-[100%] object-contain' />
             </div>
         </div>
     </div>
