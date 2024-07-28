55c792b1f95852805d950aec0b87b5605ea08d34
Nikhil Sharma <nikhilsharma7028@gmail.com>
2024-07-28 19:03:41 +0530

    Final Updates

--- Components/Services.tsx
+++ Components/Services.tsx
@@ -8,7 +8,7 @@ const Services = () => {
             My <span className='text-yellow-400'>Services</span>
         </p>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white'>
-            <div>
+            <div data-aos="fade-right">
                 <div className='bg-red-700 transform transition-all duration-300 hover:rotate-6 hover:scale-110 uppercase font-semibold text-center p-[2rem]'>
                     <CodeBracketSquareIcon className='w-[6rem] h[6rem] mx-auto text-[#d3fae8]' />
                     <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
@@ -21,7 +21,7 @@ const Services = () => {
                     </p>
                 </div>
             </div>
-            <div>
+            <div data-aos="zoom-in" data-aos-delay="300">
                 <div className='bg-yellow-700 hover:scale-110 transform transition-all duration-all duration-300 hover-rotate-6 uppercase font-semibold text-center p-[2rem]'>
                     <CodeBracketSquareIcon className='w-[6rem] h[6rem] mx-auto text-[#d3fae8]' />
                     <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
@@ -34,7 +34,7 @@ const Services = () => {
                     </p>
                 </div>
             </div>
-            <div>
+            <div data-aos="fade-left" data-aos-delay="500">
                 <div className='bg-blue-700 hover:scale-110 transform transition-all duration-all duration-300 hover-rotate-6 uppercase font-semibold text-center p-[2rem]'>
                     <CodeBracketSquareIcon className='w-[6rem] h[6rem] mx-auto text-[#d3fae8]' />
                     <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
